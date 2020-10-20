d008de4656598b71b3dcc5a614a15e2b21e322fc
Tim Emiola <adetokunbo@users.noreply.github.com>
2020-10-20 16:10:06 +0900

    By default build for the pinned version of ghc865

--- build.nix
+++ build.nix
@@ -1,27 +1,34 @@
 { system ? builtins.currentSystem
-, nixBase ? "18.09"
+, nixBase ? "lts-haskell.14.0"
 }:
 let
   overridez = import ./nix/haskell-overridez.nix;
-  pkgsMakeHaskellOverridez = pkgs: overridez.allIn ./nix;
   pkgsMake = import ./nix/fetchPkgsMake.nix {};
+  nixpkgsVersionFile = (./. + "/nix/${nixBase}.nix");
+  nixpkgsVersion = import nixpkgsVersionFile;
+
+  pkgsMakeHaskellOverridez = pkgs: overridez.allIn ./nix;
   pkgsMakeArgs = {
-    nixpkgsRev = "a4c4cbb613cc3e15186de0fdb04082fa7e38f6a0";
-    nixpkgsSha256 = "1lagfycy2lvfc8cdxk98dz2rxjlrbmv9hj42x0x40sy66bck1w0y";
+    nixpkgsRev = nixpkgsVersion.rev;
+    nixpkgsSha256 = nixpkgsVersion.sha256;
     haskellArgs = {
+      ghcVersion = "ghc865";
       overrides = pkgsMakeHaskellOverridez;
       extraOverrides = pkgsMakeHaskellOverridez;
       envMoreTools = [
+        pkgs.haskellPackages.apply-refact
         pkgs.haskellPackages.cabal2nix
         pkgs.haskellPackages.cabal-install
         pkgs.haskellPackages.ghcid
+        pkgs.haskellPackages.ghci-pretty
         pkgs.haskellPackages.hlint
         pkgs.haskellPackages.hoogle
+        pkgs.haskellPackages.stylish-cabal
         pkgs.haskellPackages.stylish-haskell
       ];
     };
   };
-  pkgs = import ./nixpkgs.nix { inherit nixBase system; };
+  pkgs = import ./nix/fetchNixPkgs.nix { inherit nixpkgsVersionFile; };
 
 in
 
