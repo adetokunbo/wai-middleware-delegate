f9e44f1116071c62b9932710499fcfa50cb8331b
Tim Emiola <adetokunbo@users.noreply.github.com>
2021-09-29 17:20:08 +0900

    Switch to using the haskell.nix build tools
    - this also switches to dev building with GHC 8.10.5

--- nix/lts-haskell.14.0.nix
+++
@@ -1,4 +0,0 @@
-{
-  rev = "99423d71c8fdd0319acd8112c4df9f3a086eff3e";
-  sha256 = "1g32kzsmd15448g8g8b1j9sc6ggsxmjd043zj8rg8c8ljqaji2i3";
-}
--- nixpkgs.nix
+++
@@ -1,8 +0,0 @@
-{ system ? builtins.currentSystem,
-  nixBase ? "18.09"
-}:
-let
-  nixPkgsPath = ./nix + "/${nixBase}.nix";
-  nixpkgs = import nixPkgsPath;
-in
-  import nixpkgs { inherit system; }
