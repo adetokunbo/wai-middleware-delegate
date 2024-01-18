2cedf7f1e2bc81341c9098aed166171775a6394e
Tim Emiola <adetokunbo@users.noreply.github.com>
2024-01-18 10:56:51 +0900

    Updated the nix pin to a commit on 2023/11/23

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  project = import ./default.nix;
+  project = import ./default.nix {};
 in
   project.shellFor {
     # Builds a Hoogle documentation index of all dependencies,
@@ -12,7 +12,7 @@ in
       hlint = "latest"; # Selects the latest version in the hackage.nix snapshot
       haskell-language-server = "latest";
       ghcid = "latest";
-      fourmolu = "latest";
+      fourmolu = "0.14.0.0";
       cabal-fmt = "latest";
     };
     # See overlays/tools.nix for more details
